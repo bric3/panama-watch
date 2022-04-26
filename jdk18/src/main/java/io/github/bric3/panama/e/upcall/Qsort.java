3a650d7b97d104a57d8b23c00b33e991ef4825e4
Brice Dutheil <brice.dutheil@gmail.com>
2022-04-27 00:10:22 +0200

    feat: Relicense to MPL
    Code unlikely to go to prod as-is, but the idea is to incite to contribute back.

--- jdk18/src/main/java/io/github/bric3/panama/e/upcall/Qsort.java
+++ jdk18/src/main/java/io/github/bric3/panama/e/upcall/Qsort.java
@@ -1,3 +1,12 @@
+/*
+ * panama-watch
+ *
+ * Copyright (c) 2021, Today - Brice Dutheil
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.github.bric3.panama.e.upcall;
 
 import jdk.incubator.foreign.CLinker;
