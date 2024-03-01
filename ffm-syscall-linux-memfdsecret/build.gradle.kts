b88d88b751d742e29c4baba944e1d881c000eeb1
Brice Dutheil <brice.dutheil@gmail.com>
2024-03-01 14:50:46 +0100

    chore: Fix license header and import statement order

--- ffm-syscall-linux-memfdsecret/build.gradle.kts
+++ ffm-syscall-linux-memfdsecret/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
-
 /*
  * panama-watch
  *
@@ -9,6 +7,8 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
 plugins {
   id("panama.java-conventions")
 }
--- touchid-swift-lib/build.gradle.kts
+++ touchid-swift-lib/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
-
 /*
  * panama-watch
  *
@@ -9,9 +7,7 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
-
-
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 
 plugins {
   `swift-library`
